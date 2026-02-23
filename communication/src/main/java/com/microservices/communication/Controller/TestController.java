c118f40f4476e2123155b9169732d1861ed00532
Upendra-3596 <gudariupendra9@example.com>
2026-02-23 18:11:18 +0530

    first commit

---
+++ communication/src/main/java/com/microservices/communication/Controller/TestController.java
@@ -0,0 +1,15 @@
+package com.microservices.communication.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+	@GetMapping("/tagline")
+	public String tagLine() {
+		return "your Java Stop!";
+	}
+}
